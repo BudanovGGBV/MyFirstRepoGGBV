44d406ee320ea5b1d4ad282c38c4020c00122da9
BudanovGGBV <99897308+BudanovGGBV@users.noreply.github.com>
2022-02-21 11:59:29 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp2
             Console.WriteLine("Hello World!!!!!");
             Console.WriteLine("Hello World!2!!!!");
             Console.WriteLine("Hello World!2!!!!");
+            Console.WriteLine("Hello World!2!!!!");
         }
     }
 }

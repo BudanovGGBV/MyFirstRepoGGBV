109d2c02018af6f7dfa8b92cf4f2df76ead5c715
Albert Tashu <albert.t@sbtech.com>
2022-02-21 15:03:05 +0200

    update console log

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!!!");
-            Console.WriteLine("Hello World!2!!!!");
-            Console.WriteLine("Hello World!2!!!!");
-            Console.WriteLine("Hello World!2!!!!");
+            Console.WriteLine("Hello World!!!!!!");
+            Console.WriteLine("Hello World!!!!!!");
+            Console.WriteLine("Hello World!!!!!!");
         }
     }
 }
